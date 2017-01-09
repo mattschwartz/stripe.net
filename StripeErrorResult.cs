96fc45703844baebf556e3a06b347bb90b6ab062
mattschwartz <schwamat@gmail.com>
2017-01-09 08:46:30 -0600

    Added error result

---
+++ StripeErrorResult.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Stripe.Net
+{
+    public class StripeErrorResult
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("param")]
+        public string Parameter { get; set; }
+    }
+
+    public class StripeErrorResultContainer
+    {
+        [JsonProperty("error")]
+        public StripeErrorResult Error { get; set; }
+    }
+}
---
+++ StripeListObject.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Stripe.Net
+{
+    public class StripeListObject
+    {
+        [JsonProperty("data")]
+        public List<dynamic> Data { get; set; } = new List<dynamic>();
+    }
+
+    public class StripeListObject<T>
+    {
+        [JsonProperty("data")]
+        public List<T> Data { get; set; } = new List<T>();
+    }
+}
